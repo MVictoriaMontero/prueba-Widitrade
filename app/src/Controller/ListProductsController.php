0dc75de4b9b734a2d2badfcf1bd1acca192348f0
Victoria Montero <geekvico@gmail.com>
2024-04-26 11:47:51 +0200

    Aplicar estilos. Limpieza de codigo

--- app/src/Controller/ListProductsController.php
+++ app/src/Controller/ListProductsController.php
@@ -14,10 +14,8 @@ class ListProductsController extends AbstractController
     public function index(): Response
     {
         $datasJSON = json_decode(file_get_contents(__DIR__.'/../data/data.json'),true);
-        //print_r($datasJSON.Items);
         $itemsData = $datasJSON['SearchResult']['Items'];
-        //print_r('HEEEYY %%%%: '.count($itemsData));
-        //print_r($datasJSON['SearchResult']['Items']);
+        
         return $this->render('list_products/index.html.twig', [
             'controller_name' => 'ListProductsController',
             'datas_json' => $itemsData,
